9c530dda2d151c0d97c530b782669b8fbb86b681
Do Minh Huyen Nguyen <20900220@students.ltu.edu.au>
2023-10-13 23:30:22 +1100

    New change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,30 @@ import './App.css';
 function App() {
 
     return (
-        <div>     
-            <h1>Contactor</h1>
+    <div class='wrap'> 
+        <h1 class='head'>   Contactor </h1>   
+        <div class='row'>
+        <div class ='box'>     
+            <h2 class ='head'> Contacts </h2>
+                <h3 class='head'><input type = "text" placeholder='Name'></input></h3>
+                <div class ='head'> 
+                <button type='button' class='green'>Create Contact</button>
+                </div>
+            
         </div>
+        <div class ='stat'>
+            <p>Click a contact to view associated phone numbers</p>
+            <p>Show stats</p>
+        </div>
+        </div>
+    </div>
+
+    
+        
     );
+    
+
+
 }
 
 export default App;
