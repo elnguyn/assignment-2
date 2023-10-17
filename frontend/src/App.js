142a4479f862d8dea377eaa89584df883d5ce3f0
Do Minh Huyen Nguyen <20900220@students.ltu.edu.au>
2023-10-18 01:09:54 +1100

    New

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,68 @@
 import { useState, useEffect } from 'react';  // import useEffect
-import List from './components/List';
 import './App.css';
+import Contact from './components/Contact';
 
 
 
 //Build front
 function App() {
 
-    const [contacts, setContact] = useState([]);
-    
-    useEffect(() => {
-      fetch('http://localhost/api/contacts')
-        .then(response => response.json())
-        .then(data => setContact(data))
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-      }, []);
+  const host = "http://localhost:5000/api";
+  const [contacts, setContacts] = useState([]);
+  const [newContact, setNewContact] = useState("");
 
-    
-    return(
-        <div class='head'>
-            <List heading = 'Contactor' contacts={contacts} setContact={setContact} />
-        </div>
-    );
-  
+  function changeContact(event) {
+      setNewContact(event.target.value);
+  }
+
+  function addContact() {
+      fetch(`${host}/contacts`, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ name: newContact })
+      })
+          .then(response => response.json())
+          .then(data => {
+              if (data && data.id) {
+                  setContacts(prevContacts => [...prevContacts, data]);
+              } else {
+                  console.error('Error creating contact:', data);
+              }
+          })
+          .catch(error => {
+              console.error('Error:', error);
+          });
+      setNewContact("");
+  }
+
+  useEffect(() => {
+      fetch(`${host}/contacts`)
+          .then(response => response.json())
+          .then(data => setContacts(data))
+          .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+      <div>
+          <h1>Contactor</h1>
+          <h2>Contacts</h2>
+          <div>
+              <div>
+                  <input type='text' placeholder='Name' value={newContact} onChange={changeContact}></input>
+                  <button type='button' className='green' onClick={addContact}>Create Contact</button>
+              </div>
+              {contacts.map(contact => (
+                  <Contact
+                      setContacts={setContacts} // Changed the prop name to match state variable
+                      id={contact.id}
+                      key={contact.id}
+                  />
+              ))}
+          </div>
+      </div>
+  );
   
 }
 
